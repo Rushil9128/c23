899dfe73ba0bdcc4acc950fa302dab02753cbc73
Rushil9128 <73953157+Rushil9128@users.noreply.github.com>
2020-11-18 19:53:16 +0530

    Add files via upload

---
+++ boxClass-master/Ground.js
@@ -0,0 +1,16 @@
+class Ground{
+    constructor(x,y,width,height){
+        var ground_option={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,width,height,ground_option);
+        World.add(world,this.body);
+        this.width=width;
+        this.height=height;
+    }
+    display(){
+        rectMode(CENTER);
+        fill(255);
+        rect(this.body.position.x,this.body.position.y,this.width,this.height);
+    }
+}
